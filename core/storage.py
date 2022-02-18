7088740a223d36441a0dff37ff49d241d3605207
huayong1678 <poramesjar2000@gmail.com>
2022-02-19 02:25:43 +0700

    First Commit

---
+++ core/storage.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+    def __init__(self, *args, **kwargs):
+        kwargs['custom_domain'] = settings.AWS_S3_CUSTOM_DOMAIN
+        super(StaticStorage, self).__init__(*args, **kwargs)
---
+++ users/models.py
@@ -0,0 +1,14 @@
+import email
+from re import M
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FILEDS = []
+
