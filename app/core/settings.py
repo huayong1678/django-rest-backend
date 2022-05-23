72cb0337ae5a86f34dc5486b1a1b694d27532073
huayong1678 <poramesjar2000@gmail.com>
2022-05-24 02:18:19 +0700

    Fix CORs

--- app/core/settings.py
+++ app/core/settings.py
@@ -91,7 +91,8 @@ MIDDLEWARE = [
 # ]
 
 # CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = ['https://d2l6qgsxbagd66.cloudfront.net']
+# CORS_ALLOWED_ORIGINS = ["https://d2l6qgsxbagd66.cloudfront.net", "http://127.0.0.1:3000", "http://localhost:3000"]
+CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ORIGIN_WHITELIST = ['https://d2l6qgsxbagd66.cloudfront.net']
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
